5850426df026c66c14bae135ee5a94f8b0452629
Cory <Coryjh85@gmail.com>
2017-04-23 20:35:34 -0700

    Image Carousel Working

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,28 @@
 $(function(){
 
 //SMOOTH SCROLLING
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+    return false;
+});
 
 //FLICKITY SLIDER
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
+
 
 //FORM SUBMITTING THINGER
 
